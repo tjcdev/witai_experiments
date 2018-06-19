8cd7362d88a797a62679aa4258ae2b2cdb0bcd61
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2018-06-19 19:32:06 +0100

    update the values in an entity

--- ex1.py
+++ ex1.py
@@ -1,4 +1,30 @@
 from wit import Wit
+import urllib2, json
+
+def addTeam():
+    url = 'https://api.wit.ai/entities/team/values'
+    data = {
+        'value': 'Miami Heat',
+        'expressions': ['miami heat', 'the heat']
+    }
+
+    req = urllib2.Request(url)
+    req.add_header('Authorization', 'Bearer IBRA2N7KIJLX5GUCSX64G3XE4U5VYM4A')
+    req.add_header('Content-Type', 'application/json')
+    req.get_method = lambda: 'POST'
+
+    try:
+    	response = urllib2.urlopen(req, json.dumps(data))
+    except urllib2.HTTPError as e:
+    	print "Error occured while updating existing entity"
+    	print e.code
+    	print e.read()
+    	sys.exit(1)
+
+    print "Entity correctly updated :" + response.read()
+
+
+addTeam()
 
 def getMessage():
     # the Wit access token
@@ -6,7 +32,7 @@ def getMessage():
 
     client = Wit(access_token)
 
-    response = client.message("what are the odds of England vs Germany?")
+    response = client.message("what are the odds for celtic vs rangers?")
 
     entities = response["entities"]
 
@@ -21,6 +47,3 @@ def getMessage():
 
     print(intent)
     print(teams_list)
-
-
-getMessage()
